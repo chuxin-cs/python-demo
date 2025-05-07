3fce001e365bba70b070f86f60f4aa6fa0456557
chuxin <15115359055@139.com>
2025-05-07 15:28:06 +0800

    feat(api): 添加对POST请求的支持并扩展GET请求参数
    为`/index`路由添加了对JSON参数的解析，并新增了`/submit`路由以支持POST请求，提升API的灵活性和功能完整性。

--- demo-crud/code.py
+++ demo-crud/code.py
@@ -2,17 +2,38 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/index')
+
+# http://127.0.0.1:5000/index?page=1&pageSize=20
+@app.route('/index', methods=['GET'])
 def index():
     # 获取页码，默认值为 1
     page = request.args.get('page', 1, type=int)
     # 获取每页数量，默认值为 20
     page_size = request.args.get('pageSize', 20, type=int)
     # 以字典形式返回数据，方便前端解析
+    page1 = request.json.get('page1', 1, type=int)
+    page_size1 = request.json.get('pageSize1', 20, type=int)
     return {
         "page": page,
-        "pageSize": page_size
+        "pageSize": page_size,
+        "page1": page1,
+        "pageSize1": page_size1,
     }
+
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    page = request.form.get('page', 1, type=int)
+    page_size = request.form.get('pageSize', 20, type=int)
+    return {
+        "message": "post请求的案例",
+        "status": 200,
+        "params": {
+            "page": page,
+            "pageSize": page_size
+        }
+    }
+
 
 if __name__ == '__main__':
     app.run()
