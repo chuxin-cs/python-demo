d017a4110e531b70dacf24141fbd83b8036c4ebb
chuxin <15115359055@139.com>
2025-05-28 15:06:03 +0800

    feat: 添加 YOLO 目标检测功能
    - 新增 YOLO 模型文件和相关资源图片
    - 实现 perform_object_detection 方法用于本地/网络图片及视频检测
    - 修改 app.py 主程序调用检测功能
    - 添加详细注释说明使用方法和参数配置

--- demo-lib/app.py
+++ demo-lib/app.py
@@ -1,2 +1,7 @@
+from lib.yolo import perform_object_detection
+
 if __name__ == '__main__':
     print('python lib study...')
+
+    # 预训练的 YOLO 模型，对本地图片、网络图片以及视频进行目标检测
+    perform_object_detection()
---
+++ demo-lib/lib/yolo.py
@@ -0,0 +1,25 @@
+"""
+此脚本用于加载预训练的 YOLO 模型，对本地图片、网络图片以及视频进行目标检测
+    1.运行前需要安装第三方库 ultralytics，可使用以下命令进行安装：
+        pip install ultralytics
+    2.若安装过程中出现超时问题，可使用国内镜像源进行安装，命令如下：
+        pip install -i https://mirrors.aliyun.com/pypi/simple/ ultralytics
+"""
+
+from ultralytics import YOLO
+
+"""
+执行目标检测任务
+    1. 参数:
+        url (str): 要进行检测的(图片、视频)的路径或在线URL，默认为 'https://imgcdn.cna.com.tw/Eng/WebEngPhotos/800/2025/20250527/1024x698_763747498679.jpg'
+"""
+def perform_object_detection(url = 'https://imgcdn.cna.com.tw/Eng/WebEngPhotos/800/2025/20250527/1024x698_763747498679.jpg'):
+    # 1，加载预训练模型，如果没有文件，首次运行自动下载
+    model = YOLO("yolo11n.pt")  
+
+    """
+    参数详解
+        show=True 显示检测结果图片
+        save=True 保存检测结果图片，会创建 runs 文件夹
+    """
+    model(url, show=True, save=True)
