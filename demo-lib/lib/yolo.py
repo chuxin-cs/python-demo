9e997db18f5bcdadf3e0128609ed689ee2c366dc
chuxin <15115359055@139.com>
2025-05-28 15:14:34 +0800

    feat(yolo): 添加保存检测结果目录参数
    为 perform_object_detection 函数新增 save_dir 参数，允许自定义检测结果保存路径

--- demo-lib/lib/yolo.py
+++ demo-lib/lib/yolo.py
@@ -12,14 +12,16 @@ from ultralytics import YOLO
 执行目标检测任务
     1. 参数:
         url (str): 要进行检测的(图片、视频)的路径或在线URL，默认为 'https://imgcdn.cna.com.tw/Eng/WebEngPhotos/800/2025/20250527/1024x698_763747498679.jpg'
+        save_dir (str): 保存检测结果的文件夹路径，默认为 '../assets'
 """
-def perform_object_detection(url = 'https://imgcdn.cna.com.tw/Eng/WebEngPhotos/800/2025/20250527/1024x698_763747498679.jpg'):
+def perform_object_detection(url = 'https://imgcdn.cna.com.tw/Eng/WebEngPhotos/800/2025/20250527/1024x698_763747498679.jpg', save_dir='assets'):
     # 1，加载预训练模型，如果没有文件，首次运行自动下载
     model = YOLO("yolo11n.pt")  
 
     """
     参数详解
         show=True 显示检测结果图片
-        save=True 保存检测结果图片，会创建 runs 文件夹
+        save=True 保存检测结果图片，会创建指定的文件夹
+        project=save_dir 指定保存结果的文件夹路径
     """
-    model(url, show=True, save=True)
+    model(url, show=True, save=True, project=save_dir)
